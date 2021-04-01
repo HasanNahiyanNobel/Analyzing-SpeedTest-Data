9c098d0c8764db586c5f6c7ce116e0816c2d1db3
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-04-01 18:44:37 +0600

    R now plots the data from csv to a png file. A third argument is added in shell script to turn on or off this plotting.

--- SpeedTest Data Analyzer.py
+++ SpeedTest Data Analyzer.py
@@ -5,7 +5,7 @@ file_input = open("SpeedTest Outputs.txt", "r")
 
 open("SpeedTest Down-Up Data.csv", "w").close()  # Clearing existing data
 file_output = open("SpeedTest Down-Up Data.csv", "a")
-file_output.write("Date, Time, Download Speed (Mbyte/s), Upload Speed (Mbyte/s)\n")
+file_output.write("Date-Time, Download Speed (Mbyte/s), Upload Speed (Mbyte/s)\n")
 
 for a_line in file_input:
 	json_data = json.loads(a_line)
@@ -16,7 +16,7 @@ for a_line in file_input:
 	date = timestamp[:10]  # date as yyyy-mm-dd format
 	time = timestamp[11:-8]  # time as hh:mm:ss format
 
-	string_to_write = date + ", " + time + ", " + str(download) + ", " + str(upload) + "\n"
+	string_to_write = date + " " + time + ", " + str(download) + ", " + str(upload) + "\n"
 	file_output.write(string_to_write)
 
 file_input.close()
