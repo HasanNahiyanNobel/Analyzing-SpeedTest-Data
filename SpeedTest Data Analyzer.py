952788e4fdb5dd0e46b2af09177faca6b57513e9
Hasan Nahiyan Nobel <43447022+HasanNahiyanNobel@users.noreply.github.com>
2021-04-01 16:16:07 +0600

    Timestamp in csv is now stored as date, time (separately).

--- SpeedTest Data Analyzer.py
+++ SpeedTest Data Analyzer.py
@@ -1,17 +1,22 @@
 import json
 
+
 file_input = open("SpeedTest Outputs.txt", "r")
 
 open("SpeedTest Down-Up Data.csv", "w").close()  # Clearing existing data
 file_output = open("SpeedTest Down-Up Data.csv", "a")
-file_output.write("Timestamp, Download Speed (Mbyte/s), Upload Speed (Mbyte/s)\n")
+file_output.write("Date, Time, Download Speed (Mbyte/s), Upload Speed (Mbyte/s)\n")
 
 for a_line in file_input:
 	json_data = json.loads(a_line)
 	timestamp = json_data["timestamp"]
 	download = round(json_data["download"] / 8000000, 2)
 	upload = round(json_data["upload"] / 8000000, 2)
-	string_to_write = timestamp + ", " + str(download) + ", " + str(upload) + "\n"
+
+	date = timestamp[:10]  # date as yyyy-mm-dd format
+	time = timestamp[11:-8]  # time as hh:mm:ss format
+
+	string_to_write = date + ", " + time + ", " + str(download) + ", " + str(upload) + "\n"
 	file_output.write(string_to_write)
 
 file_input.close()
